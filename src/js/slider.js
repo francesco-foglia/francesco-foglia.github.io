d8db765fbd11206bbbac439720447c51092e7126
Francesco Foglia <francescofogliajj@gmail.com>
2023-01-13 20:29:47 +0100

    modified:   css/app.css new file:   img/projects/art-partner.png new file:   img/projects/capellimario.png new file:   img/projects/curlybags.png new file:   img/projects/diletteratura.png new file:   img/projects/frantoioilmandorlosrl.png new file:   img/projects/html-css-bootstrap-freelancer.png new file:   img/projects/html-css-digitalocean.png new file:   img/projects/html-css-spotifyweb.png new file:   img/projects/htmlcss-hubspot.png new file:   img/projects/htmlcss-wp.png new file:   img/projects/js-minesweeper.png new file:   img/projects/proj-html-vuejs.png new file:   img/projects/vue-boolflix.png new file:   img/projects/vue-boolzapp.png new file:   img/projects/vue-dischi.png modified:   index.html modified:   js/app.js modified:   src/js/slider.js modified:   src/sass/partials/_slider.scss

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,22 +1,45 @@
+const swiperSlide = [
+  { name: "htmlcss-hubspot", link: "https://francesco-foglia.github.io/htmlcss-hubspot/" },
+  { name: "html-css-digitalocean", link: "https://francesco-foglia.github.io/html-css-digitalocean/" },
+  { name: "html-css-spotifyweb", link: "https://francesco-foglia.github.io/html-css-spotifyweb/" },
+  { name: "vue-boolzapp", link: "https://francesco-foglia.github.io/vue-boolzapp/" },
+  { name: "vue-boolflix", link: "https://francesco-foglia.github.io/vue-boolflix/" },
+  { name: "proj-html-vuejs", link: "https://francesco-foglia.github.io/proj-html-vuejs/" },
+  { name: "art-partner", link: "https://francesco-foglia.github.io/art-partner/" },
+  { name: "js-minesweeper", link: "https://francesco-foglia.github.io/js-minesweeper/" },
+  { name: "frantoioilmandorlosrl", link: "https://www.frantoioilmandorlosrl.it/" },
+  { name: "capellimario", link: "https://www.capellimario.it/" },
+  { name: "curlybags", link: "https://www.curlybags.it/" },
+  { name: "diletteratura", link: "https://www.diletteratura.it/" },
+  { name: "htmlcss-wp", link: "https://francesco-foglia.github.io/htmlcss-wp/" },
+  { name: "html-css-bootstrap-freelancer", link: "https://francesco-foglia.github.io/html-css-bootstrap-freelancer/" },
+  { name: "vue-dischi", link: "https://francesco-foglia.github.io/vue-dischi/" },
+];
+
+swiperWrapper = document.querySelector('.swiper-wrapper');
+
+swiperSlide.forEach((slide) => {
+  swiperWrapper.innerHTML += `
+    <div class="swiper-slide">
+      <a href="${slide.link}" target="_blank">
+        <img src="img/projects/${slide.name}.png" alt="${slide.name}">
+      </a>
+    </div>
+  `;
+});
+
 const swiper = new Swiper('.swiper', {
   effect: "fade",
-  // Optional parameters
-  direction: 'horizontal',
+  speed: 500,
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
